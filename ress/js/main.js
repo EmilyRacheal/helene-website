ffb7cb7671d1e9160b055e1cbfe779fc78b833cb
EmilyRacheal <emilychealy4@gmail.com>
2021-06-15 17:04:10 +0100

    eddited somethings

--- ress/js/main.js
+++ ress/js/main.js
@@ -160,13 +160,6 @@
     $('.venobox').venobox();
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
 
   // Init AOS
   function aos_init() {
